19f90d3145550d5a7090dd37efb43e982dad5494
Pablo <pabloms.94@usal.es>
2016-05-15 12:45:19 +0200

    Integrales dobles, interpolación, exportación a JSON

--- src/integralesV2.c
+++ src/integralesV2.c
@@ -11,17 +11,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "cJSON.h"
 
-float integral (float menor, float particiones, float dx);
 float dobleIntegral (float menor1, float mayor2, float menor2, float particiones, float dx);
 float funcion (float x, float y);
+double newton (int n, double *x, double *y, double interpolar);
+void create_objects(double *x, double *y, int i);
 
 /**
- * Menor y mayor hacen referecia a los extremos de las integrales.
- * El uno indica que es de la integral externa, y el 2 de la interna.
+ * @brief main
+ * @param menor hace referecia al extremo inferior de las integrales.
+ * @param mayor hace referecia al extremo superior de las integrales.
+ * @warning uno y dos situado en el nombre de los parametros diferencia si los extremos pertenecen a la integral externa o interna.
  */
 
 int main(void) {
+	//Variables usadas para las integrales.
 	float menor1 = 1;
 	float mayor1 = 2;
 	float menor2 = 3;
@@ -30,31 +35,45 @@ int main(void) {
 	float dy;
 	float resultado;
 
+	//Variables usadas para la interpolación
+	/*float x[] = {-3, -2, 0, 4};
+	float y[] = {5, 8, 4, 2};
+	*/
+
+	double energia[60];
+	double atenuacion[60];
+	double absorcion[60];
+	int i = 0;
+	double energiaInterpolar = 20;
+	double interpolacion;
+
+	FILE* f= fopen("../mu/mu74.csv","r");
+
+		while (fscanf(f,"%lg,%lg,%lg\n",&energia[i],&atenuacion[i],&absorcion[i])!=EOF){
+			printf("Lectura: a=%lf, b=%lf,c=%lf\n",energia[i],atenuacion[i],absorcion[i]);
+			i++;
+			printf("i = %d\n", i);
+	}
+	fclose(f);
+
+	//Integrales
 	dy = (mayor1 - menor1) /particiones;
 	resultado = dobleIntegral(menor1, mayor2, menor2, particiones, dy);
-	printf ("RESULTADO : %f", resultado);
+	printf ("RESULTADO INTEGRALES: %f.\n\n", resultado);
+
+	//Interpolación
+	interpolacion = newton (i, energia, atenuacion, energiaInterpolar);
+	printf ("RESULTADO INTERPOLACIÓN: %lg.\n\n", interpolacion);
+
+	create_objects(energia, atenuacion, i);
 
 	return 0;
 }
-/*
-float integral (float menor, float particiones, float dx){
-	int i;
-	float verticeMenor;
-	float valorFuncion;
-	float area;
-	float resultado = 0;
 
-	for(i = 0; i < particiones; i++){
-		verticeMenor = menor + i*dx;
-		valorFuncion = funcion(verticeMenor);
-		area = valorFuncion * dx;
-		resultado += area;
-	}
-
-	return resultado;
-}*/
 /**
- * Los valores verticeMenor hacen referencia al valor del extremo inferior de la partición que se toma como referencia.
+ * @brief dobleIntegral calcula la doble integral que se le pasa a la funcion.
+ * \n\n VARIABLES
+ * @param verticeMenor hace referencia al valor del extremo inferior de la particion que se toma como referencia. X e Y distinguen la coordenada que almacenan.
  */
 float dobleIntegral (float menor1, float mayor2, float menor2, float particiones, float dy){
 	int i, j;
@@ -84,6 +103,77 @@ float dobleIntegral (float menor1, float mayor2, float menor2, float particiones
 	return resultado1;
 }
 
+
+/*
+ * @brief funcion define la funcion que calculamos en la integral
+ */
 float funcion (float x, float y){
 	return x*y;
+}
+
+/**
+ * @brief newton calcula interpolaciones siguiendo el metodo Newton.
+ * \n\n VARIABLES
+ * @param difDiv es la tabla que contiene las diferencias divididas necesarias (coeficientes polinomio).
+ */
+double newton (int n, double *x, double *y, double interpolar){
+	double difDiv[n][n];
+	double termino;
+	double resultado;
+	int i, j;
+
+	for (i = 0; i < n; i++){
+		difDiv[i][0] = y[i];
+	}
+
+	for (j = 1; j < n; j++){
+		for (i = 0; i < n-j; i++){
+			difDiv[i][j] = (difDiv[i+1][j-1] - difDiv[i][j-1])/(x[i+j] - x[i]);
+		}
+	}
+
+	termino = 1;
+	resultado = difDiv[0][0];
+	for (i = 0; i < n-1; i++){
+		termino = termino*(interpolar - x[i]);
+		resultado = resultado + difDiv[0][i+1] * termino;
+	}
+
+	return resultado;
+}
+
+/*
+ * @brief create_objects crea archivos json para exportar los datos a java
+ * @param x es el vector de energía que se representara en el eje de las X
+ * @param y es el vector de atenuacion que se representara en el eje de las Y
+ * @param i indica el numero de elementos de los arrays
+ */
+
+void create_objects(double *x, double *y, int i)
+{
+	FILE *fp;
+	cJSON *root;
+	char *out;
+
+	fp = fopen ("x.json", "w");
+
+	root = cJSON_CreateDoubleArray(x,i);
+	out=cJSON_Print(root);
+	fputs(out, fp);
+	cJSON_Delete(root);
+	printf("%s\n",out);
+	free(out);
+	fclose(fp);
+
+	fp = fopen("y.json", "w");
+	root = cJSON_CreateDoubleArray(y,i);
+
+	out=cJSON_Print(root);
+	fputs(out, fp);
+	cJSON_Delete(root);
+	printf("%s\n",out);
+	free(out);
+
+	fclose(fp);
+
 }
